8802d4a9e695bc5d0f196273c5d5697f3e168374
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-03-15 22:04:44 +0530

    Initial Commit

---
+++ Pattern1.java
@@ -0,0 +1,22 @@
+package JavaPattern2;
+import java.util.Scanner;
+public class Pattern1 {
+public static void main(String[] args) {
+	
+	             int i,j,k;
+	             System.out.println("Enter number of rows");
+				 Scanner sc = new Scanner(System.in);
+			     
+			     k = sc.nextInt();
+			    
+			     for(i=1; i<=4; i++)
+			     {
+			     for(j=1; j<=5; j++)
+			     {
+			     System.out.print("*");
+			     }
+			     System.out.println(" ");
+				
+    	   } 
+ }
+}
